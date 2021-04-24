4772f0ecd6c696f6d9157db66bcd68c178800bad
DannJohnrem <jonrhem10@gmail.com>
2021-04-24 20:58:44 +0800

    commit changes

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app =  Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            url: "https://www.facebook.com/rhem.deguzman.quirong"
         }
     }
 })
