abcdb4e10a3693f47976dd95206666d1efc4eb73
DannJohnrem <jonrhem10@gmail.com>
2021-04-24 19:53:08 +0800

    added new files

---
+++ main.js
@@ -0,0 +1,8 @@
+const app =  Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'winter-weight knit to keep your feet warm in the colder months'
+        }
+    }
+})
