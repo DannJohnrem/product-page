899c844aa39be9afe92c0ffbd33d9e6577283735
DannJohnrem <jonrhem10@gmail.com>
2021-04-24 23:04:27 +0800

    added js folder

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app =  Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: "https://www.facebook.com/rhem.deguzman.quirong"
+        }
+    }
+})
