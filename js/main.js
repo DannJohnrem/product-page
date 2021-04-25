7981f750234516477e2275be38c9d77394ee4fbe
DannJohnrem <jonrhem10@gmail.com>
2021-04-25 16:28:12 +0800

    added a new line of code in list rendering

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-const app =  Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            varients: [{
+                    id: 2234,
+                    color: 'green'
+                },
+                {
+                    id: 2235,
+                    color: 'blue'
+                }
+            ],
         }
     }
 })
