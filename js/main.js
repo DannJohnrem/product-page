bfb23a320656f5685be969ba22a03169abccb8a8
DannJohnrem <jonrhem10@gmail.com>
2021-04-25 09:43:28 +0800

    remove URL code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const app =  Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: "https://www.facebook.com/rhem.deguzman.quirong"
         }
     }
 })
